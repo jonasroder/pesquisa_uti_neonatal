13e9f581dd0806efc8b745e19394e97d45361f06
Jonas Roder <jonasroder@gmail.com>
2023-11-15 17:10:15 -0300

    Rotas funcionais em modo produção

---
+++ src/main/java/com/roderly/microbiomelabufu/RouteForwardingController.java
@@ -0,0 +1,12 @@
+package com.roderly.microbiomelabufu;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RouteForwardingController {
+    @RequestMapping(value = "/{path:[^.]*}")
+    public String redirect() {
+        return "forward:/";
+    }
+}
