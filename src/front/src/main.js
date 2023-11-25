8b3657505727cf05f8b02fbd6cdc4d73144a8475
Jonas Roder <jonasroder@gmail.com>
2023-11-25 11:18:00 -0300

    pagina de login

--- src/front/src/main.js
+++ src/front/src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './styles/main.scss';
 import '@fortawesome/fontawesome-free/css/all.css'
+
 
 
 createApp(App).use(router).mount('#app')
--- src/front/src/views/login/Login-Page.vue
+++ src/front/src/views/login/Login-Page.vue
@@ -3,71 +3,98 @@
     import {ref, onMounted, reactive} from "vue";
 
     // reactive state
-    const usuario = ref('');
-    const senha = ref('');
     const login = reactive({Usuario: "", Senha: ""});
     const msg = ref('');
-
 
     // lifecycle hooks
     onMounted(async () => {
        // msg.value = await serviceAuthenticateUser(10);
+        console.log(msg)
+       await submitLogin()
     })
 
 
     // functions that mutate state and trigger updates
     const submitLogin = async () => {
         const res = await serviceAuthenticateUser(login)
-        msg.value=res;
-        console.log(res)
-        console.log(usuario.value + senha.value);
-        console.log(login.Usuario + " " + login.Senha)
+        console.log(login.Usuario + " " + login.Senha + res)
     }
 
 
 </script>
 
 <template>
-    <div class="hello">
-        <h1 class="row">{{ msg }}</h1>
-        Senha: {{senha}}
-        Usuario: {{usuario}}
 
-        <hr>
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="../../assets/logo_grande.png" alt="Image" class="img-fluid">
+                </div>
 
-        {{login.Usuario}}
-        <br>
-        {{login.Senha}}
+                <div class="col-md-6 contents  d-flex align-items-center">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
 
-        <div>
-            <i class="fas fa-heart"></i> <!-- Ícone de coração do Font Awesome -->
-            <i class="fas fa-star"></i> <!-- Ícone de estrela do Font Awesome -->
-        </div>
+                            <div class="mb-4">
+                                <h3 class="text-center">Bem-vindo ao Portal de Pesquisa Saúde em Equilíbio</h3>
+                                <p class="mb-4">Por favor, entre com seu usuário e senha para acessar o banco de dados de pesquisa.</p>
+                            </div>
 
-        <div class="col-md-3 mt-2">
-            <input type="text" name="Usuario" id="Usuario" placeholder="Usuário" v-model="login.Usuario">
-            <input type="text" name="Senha" id="Senha" placeholder="Senha" v-model="login.Senha">
-            <button type="submit" @click="submitLogin">Login</button>
+                            <div>
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="username" placeholder="Usuário" v-model="login.Usuario">
+                                    <label for="username">Usuário</label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input type="password" class="form-control" id="password" placeholder="Senha" v-model="login.Senha">
+                                    <label for="password">Senha</label>
+                                </div>
+                            </div>
+
+                            <input type="submit" value="Fazer login" class="btn btn-block btn-primary"  @click="submitLogin">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+
 </template>
 
 
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-    margin: 40px 0 0;
-}
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-a {
-    color: #42b983;
-}
+<style  lang="scss">
+    $font-family-sans-serif-2: 'Roboto', sans-serif;
+    $font-family-serif: 'Source Serif Pro', serif;
+    $font-family-sans-serif: 'Roboto', sans-serif;
+
+    .btn-block {
+        display: block;
+        width: 100%;
+    }
+
+    body {
+        font-family: $font-family-sans-serif;
+        background-color: #f8fafb;
+    }
+
+    p {
+        color: darken(#ccc, 10%);
+        font-weight: 300;
+    }
+
+    .content {
+        padding: 7rem 0;
+    }
+
+    .content {
+        .btn {
+            height: 54px;
+            padding-left: 30px;
+            padding-right: 30px;
+        }
+    }
+
 </style>
