6b0605a25868ca82b41b70055f969f7cdc5e36aa
Jonas Roder <jonasroder@gmail.com>
2023-11-27 08:32:52 -0300

    Login e atenticação

--- src/main/java/com/roderly/microbiomelabufu/infra/RouteForwardingController.java
+++ src/main/java/com/roderly/microbiomelabufu/infra/RouteForwardingController.java
@@ -1,12 +1,13 @@
 package com.roderly.microbiomelabufu.infra;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RouteForwardingController {
     @RequestMapping(value = "/{path:[^.]*}")
-    public String redirect() {
+    public String redirect(@PathVariable String path) {
         return "forward:/";
     }
 }
--- src/main/java/com/roderly/microbiomelabufu/infra/security/SecurityConfiguration.java
+++ src/main/java/com/roderly/microbiomelabufu/infra/security/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.roderly.microbiomelabufu.infra.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -14,6 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -22,6 +24,7 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@CrossOrigin
 public class SecurityConfiguration {
 
     @Autowired
@@ -32,14 +35,13 @@ public class SecurityConfiguration {
         return httpSecurity
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize->authorize
+                .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/api/authenticate/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/authenticate/register").permitAll()
-                        //.requestMatchers(HttpMethod.POST, "/product").hasRole("ROLE_ADMIN")
-                        .anyRequest().authenticated()
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
                 )
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
 
