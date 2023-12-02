8411cc5ddd7afb7ca564b12920665cfc5e43cf04
Jonas Roder <jonasroder@gmail.com>
2023-12-02 18:01:26 -0300

    Login e atenticação

--- src/front/src/components/AvatarImageInput.vue
+++ src/front/src/components/AvatarImageInput.vue
@@ -1,17 +1,20 @@
 <template>
     <div>
-        <!-- Input de arquivo oculto -->
         <input type="file" @change="onFileChange" ref="fileInput" style="display: none" />
 
-        <!-- Avatar que, ao ser clicado, aciona o input de arquivo -->
         <v-avatar v-if="image" :size="180" @click="pickFile" cover>
             <v-img :src="image" cover></v-img>
         </v-avatar>
 
-        <!-- Placeholder do avatar se nenhuma imagem for selecionada -->
         <v-avatar v-else :size="180" @click="pickFile" color="grey lighten-4">
             <v-icon size="32" class="fa fa-camera"></v-icon>
         </v-avatar>
+
+        <!-- Botão para ativar a webcam -->
+        <v-btn @click="activateWebcam">Ativar Webcam</v-btn>
+
+        <!-- Elemento de vídeo para a webcam -->
+        <video ref="video" style="display: none;"></video>
     </div>
 </template>
 
@@ -19,12 +22,12 @@
 export default {
     data() {
         return {
-            image: null, // Para armazenar a imagem
+            image: null,
+            stream: null, // Stream da webcam
         };
     },
     methods: {
         pickFile() {
-            // Aciona o clique no input de arquivo
             this.$refs.fileInput.click();
         },
         onFileChange(e) {
@@ -37,6 +40,26 @@ export default {
                 this.image = e.target.result;
             };
             reader.readAsDataURL(file);
+        },
+        async activateWebcam() {
+            try {
+                this.stream = await navigator.mediaDevices.getUserMedia({ video: true });
+                this.$refs.video.srcObject = this.stream;
+                this.$refs.video.play();
+                // Poderia adicionar um botão para capturar a imagem aqui
+            } catch (err) {
+                console.error("Erro ao acessar a webcam:", err);
+            }
+        },
+        // Método para capturar a imagem da webcam
+        captureFromWebcam() {
+            const canvas = document.createElement("canvas");
+            canvas.width = this.$refs.video.videoWidth;
+            canvas.height = this.$refs.video.videoHeight;
+            canvas.getContext("2d").drawImage(this.$refs.video, 0, 0);
+            this.image = canvas.toDataURL("image/png");
+            // Encerra o stream da webcam
+            this.stream.getTracks().forEach(track => track.stop());
         },
     },
 };
