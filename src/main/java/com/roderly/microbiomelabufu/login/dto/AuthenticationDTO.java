aec14483ce211529c4c0953269e8ad03080b534b
Jonas Roder <jonasroder@gmail.com>
2023-12-08 08:05:35 -0300

    Revert "implantacao migration"
    This reverts commit c90a5b5529f75edbffcfd61404abf324b71c3761.

--- src/main/java/com/roderly/microbiomelabufu/login/dto/AuthenticationDTO.java
+++ src/main/java/com/roderly/microbiomelabufu/login/dto/AuthenticationDTO.java
@@ -1,6 +1,4 @@
 package com.roderly.microbiomelabufu.login.dto;
 
-import jakarta.validation.constraints.NotBlank;
-
-public record AuthenticationDTO(@NotBlank String usuario, @NotBlank String senha) {
+public record AuthenticationDTO(String usuario, String senha) {
 }
